f89adfcdca4d118c19a79e8fb7204556b71be307
M LOW <mlow991@users.noreply.github.com>
2016-06-07 00:12:21 -0700

    feat: Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ffsiteApp
+ * @description
+ * # ffsiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ffsiteApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ffsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ffsiteApp
+ */
+angular.module('ffsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
